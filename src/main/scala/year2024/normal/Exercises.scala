f8f011f1c75019e242eb93350e865c4a924da8ad
tomasmfc <tomasmfcancela@gmail.com>
2024-07-10 08:21:17 +0100

    Add exercise 2

--- src/main/scala/year2024/normal/Exercises.scala
+++ src/main/scala/year2024/normal/Exercises.scala
@@ -19,16 +19,31 @@ def fusao(ll: List[List[Prova]], p: Prova): List[List[Prova]] =
 
   // Adiciono a prova a cada uma das listas à qual ela tem recursos em comum
 
-  ll.foreach(lp => {
+  val updatedList = ll.map(lp => {
     val recursos = lp.flatMap(_.lr)
-    println(recursos)
     if (recursos.exists(r => p.lr.contains(r)))
       p :: lp
+    else lp
   })
+
+  // Criar uma lista só com as listas que contenham a prova P e juntá-las numa só
+
+  val listWithP =
+    updatedList.filter(list => list.contains(p)).flatten.distinct
+
+  // Criar uma lista de listas que não contenham a prova P para manter a ordem
+
+  val listWithoutP =
+    updatedList.filter(list => !list.contains(p))
+
+  // Juntar as últimas duas listas caso a lista com a prova P não seja vazia
+
+  val finalList =
+    if (listWithP.isEmpty) listWithoutP else listWithP :: listWithoutP
 
   // No final, verifico se a prova de input acabou por ser adicionada. Se não foi, crio uma lista com esta prova e adiciono-a à lista de listas
 
-  if (!ll.exists(lp => lp.contains(p)))
-    List(p) :: ll
+  if (!finalList.exists(lp => lp.contains(p)))
+    List(p) :: finalList
   else
-    ll
+    finalList
